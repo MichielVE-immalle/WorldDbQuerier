1f10378b304c89eb8f2f7e13c171dab56d889ded
Michiel Van Eester <michiel.vaneester@immalle.eu>
2017-09-28 10:01:46 +0200

    version 0.3 (in progress)

--- World_Db_Querier/Program.cs
+++ World_Db_Querier/Program.cs
@@ -5,8 +5,41 @@ namespace World_Db_Querier
 {
     class Program
     {
-        static string version = "0.1";
+        static string version = "0.2";
 
+        static void TotalCountry()
+        {
+            MySqlConnection conn = new MySqlConnection();
+            conn.ConnectionString =
+                "Server=192.168.56.101;Port=3306;Database=Concerten;Uid=Imma;Pwd=imma;";
+
+            MySqlCommand cmd = new MySqlCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = "SELECT count(*) FROM world.Country";
+
+            conn.Open();
+
+            int output = Convert.ToInt32(cmd.ExecuteScalar());
+
+            Console.WriteLine("Aantal landen : {0}", output);
+        }
+
+        static void PrintCountry()
+        {
+            MySqlConnection conn = new MySqlConnection();
+            conn.ConnectionString =
+                "Server=192.168.56.101;Port=3306;Database=Concerten;Uid=Imma;Pwd=imma;";
+
+            MySqlCommand cmd = new MySqlCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = "SELECT name FROM world.Country";
+
+            conn.Open();
+
+            int output = Convert.ToInt32(cmd.ExecuteScalar());
+
+            Console.WriteLine(" landen : {0}", output);
+        }
 
         static void Main(string[] args)
         {
@@ -22,20 +55,6 @@ namespace World_Db_Querier
                         break;
                 }
             }
-
-            MySqlConnection conn = new MySqlConnection();
-            conn.ConnectionString =
-                "Server=192.168.56.101;Port=3306;Database=Concerten;Uid=Imma;Pwd=imma;";
-
-            MySqlCommand cmd = new MySqlCommand();
-            cmd.Connection = conn;
-            cmd.CommandText = "SELECT count(*) FROM world.Country";
-
-            conn.Open();
-
-            int aantalArtiesten = Convert.ToInt32(cmd.ExecuteScalar());
-
-            Console.WriteLine("Aantal landen : {0}", aantalArtiesten);
         }
     }
 }
