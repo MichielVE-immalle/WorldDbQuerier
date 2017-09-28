c96983921902a304929e2a42208a22d5816a6fd3
Michiel Van Eester <michiel.vaneester@immalle.eu>
2017-09-28 09:40:04 +0200

    Code added, working connection SqlServer

--- World_Db_Querier/Program.cs
+++ World_Db_Querier/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using MySql.Data.MySqlClient;
 
 namespace World_Db_Querier
 {
@@ -21,10 +22,20 @@ namespace World_Db_Querier
                         break;
                 }
             }
-            else
-            {
-                Console.WriteLine("Hello World");
-            }
+
+            MySqlConnection conn = new MySqlConnection();
+            conn.ConnectionString =
+                "Server=192.168.56.101;Port=3306;Database=Concerten;Uid=Imma;Pwd=imma;";
+
+            MySqlCommand cmd = new MySqlCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = "SELECT count(*) FROM Artiesten";
+
+            conn.Open();
+
+            int aantalArtiesten = Convert.ToInt32(cmd.ExecuteScalar());
+
+            Console.WriteLine("Aantal artiesten : {0}", aantalArtiesten);
         }
     }
 }
